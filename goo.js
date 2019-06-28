dbcc85f407e18a0294bb8a567e8f9814c37003b0
dinagouda <dinagouda1@gmail.com>
2019-06-29 01:04:26 +0200

    first

---
+++ goo.js
@@ -0,0 +1,35 @@
+window.onload=function(){
+//alert("kh");
+
+document.getElementById("clic").onclick=getLocation;
+
+function showLocation(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    var latlongvalue = position.coords.latitude + ","
+    + position.coords.longitude;
+    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlongvalue+"&amp;zoom=14&amp;size=400x300&amp;key=AIzaSyAa8HeLH2lQMbPeOiMlM9D1VxZ7pbGQq8o";
+    document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
+ }
+ function errorHandler(err) {
+    if(err.code == 1) {
+       alert("Error: Access is denied!");
+    } else if( err.code == 2) {
+       alert("Error: Position is unavailable!");
+    }
+ }
+
+
+ function getLocation(){
+    if(navigator.geolocation){
+       // timeout at 60000 milliseconds (60 seconds)
+       var options = {timeout:60000};
+       navigator.geolocation.getCurrentPosition
+       (showLocation, errorHandler, options);
+    } else{
+       alert("Sorry, browser does not support geolocation!");
+    }
+ }
+
+
+}
